02099c56b9d877652bb6074f701b98b2ea44a451
mitsu00 <dev@ozip.my.id>
2023-08-30 03:23:43 +0700

    fix

--- ci/sync.sh
+++ ci/sync.sh
@@ -4,7 +4,7 @@
 cd ~
 
 repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest.git -b tiramisu --git-lfs -g default,-mips,-darwin,-notdefault
-git clone https://github.com/mitsu00/local_manifest.git --depth=1 -b evox-13-staging .repo/local_manifests
+git clone https://github.com/mitsu00/local_manifest.git --depth=1 -b evox-13 .repo/local_manifests
 repo sync -qc --no-clone-bundle --optimized-fetch --prune --force-sync --fail-fast -j8
 
 echo "clang check: "
